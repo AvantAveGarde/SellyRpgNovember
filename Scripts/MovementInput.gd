adf6d4c0c98436ca1328ad198b589f32aacfff0b
Colton Lee <coltonlee4407@hotmail.com>
2017-11-09 02:00:23 -0700

    Add files via upload

--- Scripts/MovementInput.gd
+++ Scripts/MovementInput.gd
@@ -3,11 +3,14 @@ extends Node
 export var moveSpeed = 200
 var velocity = Vector2()
 
-var anim = "Idle"
+var anim = IDLE
 var sprite
 var kinematic_body
 signal move
 
+enum ANIM_STATE{
+	N, NE, E, SE, S, SW, W, NW, IDLE
+	}
 #onready var combat = get_node("CombatCore")
 func set_kinematic_body(body):
 	kinematic_body = body
@@ -34,19 +37,57 @@ func process_input(delta):
 
 func process_movement(input):
 	velocity = input.normalized() * moveSpeed
-
 	#animate
-	if sprite.animation == "Idle":
-		if  velocity.y != 0 || velocity.x != 0:
-			anim = "Walk"
-			sprite.play(anim)
-			
-	elif sprite.animation == "Walk":
+
+	if velocity.x != 0:
+		if velocity.x > 0:
+			anim = E
+		else:
+			anim = W
+	if  velocity.y != 0:
+		if velocity.y > 0:
+			anim = S
+			if velocity.x > 0:
+				#anim = SE
+				pass
+			else:
+				#anim = SW
+				pass
+		else:
+			anim = N
+			if velocity.x > 0:
+				#anim = NE
+				pass
+			else:
+				#anim = NW
+				pass
+		
+	elif anim != IDLE:
 		if  velocity.y == 0  && velocity.x == 0:
-			anim = "Idle"
-			sprite.play(anim)
+			anim = IDLE
 	#Movement
 	kinematic_body.move_and_slide(velocity)
+	draw_animation()
 	emit_signal("move")
+
+func draw_animation():
+	if(anim == ANIM_STATE.N):
+		sprite.play("North")
+	elif(anim == ANIM_STATE.NE):
+		sprite.play("NEast")
+	elif(anim == ANIM_STATE.E):
+		sprite.play("East")
+	elif(anim == ANIM_STATE.S):
+		sprite.play("South")
+	elif(anim == ANIM_STATE.SE):
+		sprite.play("SEast")
+	elif(anim == ANIM_STATE.SW):
+		sprite.play("SWest")
+	elif(anim == ANIM_STATE.W):
+		sprite.play("West")
+	elif(anim == ANIM_STATE.NW):
+		sprite.play("West")
+	
+	
 
 	
