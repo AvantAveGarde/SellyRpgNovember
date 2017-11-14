2bb977b52334821e0ec9b87af9b85b19e3ce3301
Azure <romanshnitser@hotmail.com>
2017-11-13 20:16:58 -0500

    CombatUI displays player health.

--- Combat/CombatUI.gd
+++ Combat/CombatUI.gd
@@ -5,26 +5,37 @@ extends Control
 #export(Texture) var air_texture
 #export(Texture) var earth_texture
 
+#export(Texture) var mouse_cursor
+
 var player
 var charge_label
-var mouse_cursor
+var health_label
 var viewport
+var mouse_cursor
 #var charge_image
 
 func _ready():
-	charge_label = get_node("TextureRect/Label")
+	charge_label = get_node("ChargeTexture/ChargeLabel")
+	health_label = get_node("HealthTexture/HealthLabel")
 	mouse_cursor = get_node("Cursor")
 	#charge_image = get_node("TextureRect")
 	player = get_node("../Player")
 	viewport = get_viewport()
+	Input.set_mouse_mode(1)
+	#Input.set_custom_mouse_cursor(mouse_cursor)
 	
 #TODO: Change to use signals.
 func _process(delta):
 	set_charges(player.charges)
+	set_health(player.health)
+	
 	mouse_cursor.set_position(viewport.get_mouse_position())
 
 func set_charges(value):
 	charge_label.set_text(str(value))
+	
+func set_health(value):
+	health_label.set_text(str(value))
 
 #func set_charge_texure(type):
 #	match type:
