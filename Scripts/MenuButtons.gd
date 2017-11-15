effba7d012be6ae4b228192e3addac5f550fe8c5
PyroclasticMayhem <2822125+PyroclasticMayhem@users.noreply.github.com>
2017-11-14 19:27:41 -0500

    Start game function and signals

---
+++ Scripts/MenuButtons.gd
@@ -0,0 +1,18 @@
+extends VBoxContainer
+
+func _on_NewGame_pressed():
+	get_tree().change_scene("res://Stages/stage_world.tscn")
+	pass
+
+func _on_LoadGame_pressed():
+	#TODO: Load Functions
+	pass
+
+func _on_Settings_pressed():
+	pass
+
+func _on_Credits_pressed():
+	pass
+
+func _on_Exit_pressed():
+	pass
